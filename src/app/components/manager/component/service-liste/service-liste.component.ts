eff79a73f759202141e7ce8f15761f047d36e925
AnjaArisoa <119120342+AnjaArisoa@users.noreply.github.com>
2025-03-23 15:56:47 +0300

    feat:integration-insert-service

--- src/app/components/manager/component/service-liste/service-liste.component.ts
+++ src/app/components/manager/component/service-liste/service-liste.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
 import { CommonModule } from '@angular/common';
 import { ButtonModule } from 'primeng/button';
@@ -6,6 +6,8 @@ import { InputTextModule } from 'primeng/inputtext';
 import { InputNumberModule } from 'primeng/inputnumber';
 import { Dialog } from 'primeng/dialog';
 import { Select } from 'primeng/select';
+import { FormsModule } from '@angular/forms';
+import { SericesService } from '../../../../services/serices.service';
 
 @Component({
   selector: 'app-service-liste',
@@ -14,42 +16,52 @@ import { Select } from 'primeng/select';
       ButtonModule,
       InputTextModule,
       InputNumberModule,
+      FormsModule,
       Dialog,
       Select],
   templateUrl: './service-liste.component.html',
   styleUrl: './service-liste.component.scss'
 })
-export class ServiceListeComponent {
-    donnee:any[]=[];
-    test={nom :" Vidange",description:"Nettoyage",img:"/upload"};
-    visiblecheck: boolean = false;
-    visiblenew: boolean = false;
+export class ServiceListeComponent implements OnInit {
     modele = [
-        { name: "Diesel" },
-        { name: "Essence" }
-      ];
-      type = [
-        { name: "SUV" },
-        { name: "4x4" },
-        { name: "Plaisir" },
-        { name: "Camion" },
-      ];
-    constructor() {
-        this.ajouterTest();
-      }
+            { name: "Diesel" },
+            { name: "Essence" }
+        ];
+        type = [
+            { name: "SUV" },
+            { name: "4x4" },
+            { name: "Plaisir" },
+            { name: "Camion" },
+        ];
+        newService = { nom: '', description: '',img:'' };
+        services: any[] = [];
+        visiblenew: boolean = false;
+        constructor(private servicesService: SericesService) {
+        }
+        ngOnInit(): void {
+            this.loadArticles();
+        }
+        loadArticles(): void {
+            this.servicesService.getServices().subscribe(data => this.services =
+                data);
+        }
+        deleteService(id: string): void {
+            this.servicesService.deleteService(id).subscribe(() =>
+                this.loadArticles());
+        }
+        addService(): void {
+            if (this.newService.nom && this.newService.description&&this.newService.img) {
+                this.servicesService.addService(this.newService).subscribe(() => {
+                    this.loadArticles(); // Recharge la liste après ajout
+                    this.newService = { nom: '', description: '',img:'' }; // Réinitialise le formulaire
+                });
+            }
+        }
 
-      ajouterTest() {
-        this.donnee.push(this.test);
-        console.log(this.donnee);
-      }
+        visible: boolean = false;
 
-
-      showDialogCheck() {
-          this.visiblecheck = true;
-      }
-      showDialogNew() {
-        this.visiblenew = true;
-    }
-
+        showDialogNew() {
+            this.visiblenew = true;
+        }
 
 }
